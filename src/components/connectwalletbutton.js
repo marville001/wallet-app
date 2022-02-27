a48e4daf89c85dfdcc8013a556d3ca04b32cd75b
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-27 10:53:55 +0300

    Wallet app done

---
+++ src/components/connectwalletbutton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConnectWalletButton = () => {
+  return (
+    <div className='btn-container'>
+        <button className='connectwalletbutton'>Connect Wallet</button>
+    </div>
+  )
+}
+
+export default ConnectWalletButton
---
+++ src/components/projectdescription.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProjectDescription = () => {
+    return (
+        <div className="pd-container">
+            <div className="heading">
+                <h3>BLOCKNAME:</h3>
+                <h6>Project Azawa</h6>
+            </div>
+
+            <p>Construction and development of an orphanage in Azawa</p>
+
+            <div className="desc-by">
+                BY: <span>Jeff Dylan</span>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectDescription;
